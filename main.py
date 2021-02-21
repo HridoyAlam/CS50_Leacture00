abadf02ed6df2d22c7c1f30e5b6e36216caf458a
HridoyAlam <optimus5289@gmail.com>
2021-02-21 19:02:03 +0600

    score and length increase added

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+from typing import Sized
 import pygame
 from pygame.locals import *
 import time
+import random
 #from typing_extensions import runtime
 
-class Snake:
-    def __init__(self, surface):
-        self.parent_screen = surface
-        self.block = pygame.image.load("resource/block.jpg").convert()
-        self.x = 100
-        self.y = 100
-        self.direction = 'down'
+SIZE = 40
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("resource/apple.jpg").convert()
+        self.x = 120
+        self.y = 120
     
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    
+    def move(self):
+        self.x = random.randint(1,24) * SIZE
+        self.y = random.randint(1,19) * SIZE
+
+
+class Snake:
+    def __init__(self, parent_screen, length):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("resource/block.jpg").convert()
+        self.direction = 'down'
+
+        self.length = length
+        self.x = [40] * length
+        self.y = [40] * length
 
     def move_left(self):
         self.direction = 'left'
@@ -18,22 +40,26 @@ class Snake:
     def move_right(self):
         self.direction = 'right'
 
-    def move_down(self):
+    def move_up(self):
         self.direction = 'up'
     
-    def move_up(self):
+    def move_down(self):
         self.direction = 'down'
     
 
     def walk(self):
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
         if self.direction == 'left':
-            self.x -=10
+            self.x[0] -= SIZE
         if self.direction == 'right':
-            self.x +=10
+            self.x[0] += SIZE
         if self.direction == 'up':
-            self.y +=10
+            self.y[0] -= SIZE
         if self.direction == 'down':
-            self.y -=10
+            self.y[0] +=SIZE
         
         self.draw()
         
@@ -41,15 +67,49 @@ class Snake:
 
     def draw(self):
         self.parent_screen.fill((255,0,0))
-        self.parent_screen.blit(self.block, (self.x,self.y))
+
+        for i in range(self.length):
+            self.parent_screen.blit(self.image, (self.x[i], self.y[i]))
         pygame.display.flip()
+
+
+    def increase_length(self):
+        self.length +=1
+        self.x.append(-1)
+        self.y.append(-1)
+
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500,500))
-        self.snake = Snake(self.surface)
+        self.surface = pygame.display.set_mode((1000,800))
+        self.snake = Snake(self.surface, 1)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def is_collision(self, x1,y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >=y2 and y1 < y2 + SIZE:
+                return True
+            
+        return False
+
+    
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f"Score: {self.snake.length}", True, (200,200,200))
+        self.surface.blit(score,(850, 10))
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
 
 
     def run(self):
@@ -61,6 +121,7 @@ class Game:
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
+
                     if event.key ==K_LEFT:
                         self.snake.move_left()
                     if event.key == K_RIGHT:
@@ -72,8 +133,8 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.walk()
-            time.sleep(.2)
+            self.play()
+            time.sleep(.3)
 
 if __name__ == "__main__":
     
